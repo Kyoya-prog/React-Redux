11739c4e48c93b2072a18d4e834c0e368918cd69
Kyoya-prog <kyoyaprog@gmail.com>
2022-03-31 11:50:01 +0900

    storeにcountというデータを保存

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { createStore } from "redux";
+
+const initialState = {
+  count: 1,
+};
+
+const reducer = (state = initialState) => {
+  return state
+}
+
+const store = createStore(reducer);
+
+export default store;
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
