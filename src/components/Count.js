7985b7d2faed4cb9615e91eef9fac7c2bc2146a9
Kyoya-prog <kyoyaprog@gmail.com>
2022-03-31 13:07:47 +0900

    userSelector使ってみる

--- src/components/Count.js
+++ src/components/Count.js
@@ -1,16 +1,19 @@
 import React from "react";
 import { connect } from "react-redux";
+import { useSelector } from "react-redux";
 
-function Count({ count }) {
-  return (
-    <>
-      <div>Countコンポーネント:{count}</div>
-    </>
-  );
-}
-
-const mapStateToProps = (state) => {
-  return { count: state.count };
-};
-
-export default connect(mapStateToProps)(Count);
+function Count() {
+    const count = useSelector((state) => state.count);
+    const posts = useSelector((state) => state.posts);
+    return (
+      <>
+        <div>Countコンポーネント:{count}</div>
+        <ul>
+          {posts.map((post) => (
+            <li key={post.id}>{post.title}</li>
+          ))}
+        </ul>
+      </>
+    );
+  }
+  export default Count;
